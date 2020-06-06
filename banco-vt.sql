227b306461a2b3666f8931163926d267fbda8767
grupo-02 <vinicius_rx@hotmail.com>
2020-06-06 03:20:57 -0300

    arquivo de banco SQL corrigido

--- banco-vt.sql
+++ banco-vt.sql
@@ -1,1 +1,17 @@
-select * from episodios;
+-- CREATE DATABASE episodios_db; 
+/*CREATE TABLE series (
+id varchar(100) NOT NULL,
+nome varchar(100) NOT NULL,
+autor varchar(100) NOT NULL,
+criadora varchar(100) NOT NULL,
+PRIMARY KEY(id)
+); */
+
+/*CREATE TABLE episodios (
+id varchar(100) NOT NULL,
+nome varchar(100) NOT NULL,
+temporada int NOT NULL,
+nota double NOT NULL,
+PRIMARY KEY(id) 
+);
+*/
